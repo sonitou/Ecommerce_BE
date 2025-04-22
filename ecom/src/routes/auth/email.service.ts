65a17a80a0dcd2b27321af9fb01611b166b263d2
sonn30502 <sonn220619@gmail.com>
2025-04-22 14:59:19 +0700

    feat: thêm import fs và path, đọc template OTP và thay thế subject/code trong email service

--- ecom/src/routes/auth/email.service.ts
+++ ecom/src/routes/auth/email.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from 'src/shared/config'
-
+import * as fs from 'fs'
+import * as path from 'path'
+const otpTemplate = fs.readFileSync(path.resolve('src/shared/email-templates/otp.html'), { encoding: 'utf-8' })
 @Injectable()
 export class EmailService {
   private resend: Resend
@@ -10,11 +12,12 @@ export class EmailService {
   }
 
   sendOTP(payload: { email: string; code: string }) {
+    const subject = 'Mã OTP của bạn'
     return this.resend.emails.send({
       from: 'Ecommerce <no-reply@docongson.io.vn>',
       to: [payload.email],
-      subject: 'Mã OTP của bạn',
-      html: `<strong>${payload.code}</strong>`,
+      subject,
+      html: otpTemplate.replaceAll('{{subject}}', subject).replaceAll('{{code}}', payload.code),
     })
   }
 }
