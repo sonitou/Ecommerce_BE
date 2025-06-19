cf505a7edb398a0860f39299bbd612991147dbbe
sonn30502 <sonn220619@gmail.com>
2025-06-19 17:14:48 +0700

    feat(product): Tạo method update trong ProductRepo

--- ecom/src/routes/product/product.repo.ts
+++ ecom/src/routes/product/product.repo.ts
@@ -5,6 +5,7 @@ import {
   GetProductDetailResType,
   GetProductsQueryType,
   GetProductsResType,
+  UpdateProductBodyType,
 } from './product.model'
 import { ALL_LANGUAGE_CODE } from 'src/shared/constants/order.constants'
 import { ProductType } from 'src/shared/models/shared-product.model'
@@ -99,6 +100,7 @@ export class ProductRepo {
         createdById,
         ...productData,
         categories: {
+          // dùng để nối sản phẩm vào các category đã có sẵn, theo kiểu quan hệ N-N
           connect: categories.map((category) => ({ id: category })),
         },
         skus: {
@@ -132,7 +134,112 @@ export class ProductRepo {
       },
     })
   }
+
   // - updateProduct
+  async updateProduct({
+    id,
+    updatedById,
+    data,
+  }: {
+    id: number
+    updatedById: number
+    data: UpdateProductBodyType
+  }): Promise<ProductType> {
+    const { skus: dataSkus, categories, ...productData } = data
+
+    // SKU đã tồn tại trong DB nhưng không có trong data payload thì sẽ bị xóa
+    // SKU đã tồn tại trong DB nhưng có trong data payload thì sẽ được cập nhật
+    // SKY không tồn tại trong DB nhưng có trong data payload thì sẽ được thêm mới
+
+    // 1. Lấy danh sách SKU hiện tại trong DB
+    // existingSKUs = [{ id: 1, value: 'M', stock: 10 }, { id: 2, value: 'L', stock: 5 },{ id: 3, value: 'XL', stock: 0 }]
+    const existingSKUs = await this.prismaService.sKU.findMany({
+      where: {
+        productId: id,
+        deletedAt: null,
+      },
+    })
+
+    // 2. Tìm các SKUs cần xóa (tồn tại trong DB nhưng không có trong data payload)
+    // dataSkus gửi từ client: [{value: 'M', stock: 8 }, { value: 'XL', stock: 3}]
+    // skusToDelete = [{ id: 2, value: 'L', stock: 5 }]
+    const skusToDelete = existingSKUs.filter((sku) => dataSkus.every((dataSku) => dataSku.value !== sku.value))
+    const skuIdsToDelete = skusToDelete.map((sku) => sku.id)
+    // skuIdsToDelete = [2]
+
+    // 3. Mapping ID vào trong data payload
+    const skusWithId = dataSkus.map((dataSku) => {
+      const existingSku = existingSKUs.find((existingSKU) => existingSKU.value === dataSku.value)
+      return {
+        ...dataSku,
+        id: existingSku ? existingSku.id : null,
+      }
+    })
+
+    // 4. Tìm các skus để cập nhật
+    const skusToUpdate = skusWithId.filter((sku) => sku.id !== null)
+
+    // 5. Tìm các skus để thêm mới
+    const skusToCreate = skusWithId
+      .filter((sku) => sku.id === null)
+      .map((sku) => {
+        const { id: skuId, ...data } = sku
+        return {
+          ...data,
+          productId: id,
+          createdById: updatedById,
+        }
+      })
+    const [product] = await this.prismaService.$transaction([
+      // Cập nhật Product
+      this.prismaService.product.update({
+        where: {
+          id,
+          deletedAt: null,
+        },
+        data: {
+          ...productData,
+          updatedById,
+          categories: {
+            connect: categories.map((category) => ({ id: category })),
+          },
+        },
+      }),
+      // Xóa mềm các SKU không có trong data payload
+      this.prismaService.sKU.updateMany({
+        where: {
+          id: {
+            in: skuIdsToDelete,
+          },
+        },
+        data: {
+          deletedAt: new Date(),
+          deletedById: updatedById,
+        },
+      }),
+      // Cập nhật các SKU có trong data payload
+      ...skusToUpdate.map((sku) =>
+        this.prismaService.sKU.update({
+          where: {
+            id: sku.id as number,
+          },
+          data: {
+            value: sku.value,
+            price: sku.price,
+            stock: sku.stock,
+            image: sku.image,
+            updatedById,
+          },
+        }),
+      ),
+      // Thêm mới các SKU không có trong DB
+      this.prismaService.sKU.createMany({
+        data: skusToCreate,
+      }),
+    ])
+
+    return product
+  }
 
   // - deleteProduct
   async deleteProduct(
