0a87aaee3f3f54dd86e798fa601a3d52360a7397
sonn30502 <sonn220619@gmail.com>
2025-07-16 15:27:48 +0700

    refactor(payment): cập nhật guard lấy API key từ header Authorization thay vì payment-api-key

--- ecom/src/shared/guards/payment-api-key.guard.ts
+++ ecom/src/shared/guards/payment-api-key.guard.ts
@@ -5,8 +5,8 @@ import envConfig from 'src/shared/config'
 export class PaymentAPIKeyGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest()
-    const pamentAPIKey = request.headers['payment-api-key']
-    if (pamentAPIKey !== envConfig.PAYMENT_API_KEY) {
+    const paymentApiKey = request.headers['authorization']?.split(' ')[1]
+    if (paymentApiKey !== envConfig.PAYMENT_API_KEY) {
       throw new UnauthorizedException()
     }
     return true
