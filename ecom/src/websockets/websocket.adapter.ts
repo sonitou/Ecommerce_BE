b38a532142cbd530b9b60e1e530541b6accde4de
sonn30502 <sonn220619@gmail.com>
2025-07-21 08:46:51 +0700

    feat(websocket): Lifecyle và middleware Websocket

--- ecom/src/websockets/websocket.adapter.ts
+++ ecom/src/websockets/websocket.adapter.ts
@@ -1,6 +1,7 @@
 import { IoAdapter } from '@nestjs/platform-socket.io'
 import { ServerOptions, Server, Socket } from 'socket.io'
 
+const namespaces = ['payment', 'chat']
 export class WebsocketAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions) {
     const server: Server = super.createIOServer(port, {
@@ -10,6 +11,23 @@ export class WebsocketAdapter extends IoAdapter {
         credentials: true,
       },
     })
+    const authMiddleware = (socket: Socket, next: (err?: any) => void) => {
+      console.log('connected', socket.id)
+      socket.on('disconnect', () => {
+        console.log(`Client disconnected, ${socket.id}`)
+      })
+      next()
+    }
+
+    server.use(authMiddleware)
+    server.of(/.*/).use(authMiddleware)
+
+    // namespaces.forEach((item) => {
+    //   server.of(item).use(authMiddleware)
+    // })
+    // server.use(authMiddleware)
+    // server.of('payment').use(authMiddleware)
+    // server.of('chat').use(authMiddleware)
     return server
   }
 }
