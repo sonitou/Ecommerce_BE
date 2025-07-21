11cdfdb8bfbbe45932be4b3438ff1bae4801654a
sonn30502 <sonn220619@gmail.com>
2025-07-21 17:27:11 +0700

    feat(payment): emit sự kiện về cho client khi thanh toán thành công

--- ecom/src/routes/order/order.model.ts
+++ ecom/src/routes/order/order.model.ts
@@ -42,7 +42,7 @@ export const CreateOrderBodySchema = z
   )
   .min(1)
 
-export const CreateOrderResSchema = z.object({ orders: z.array(OrderSchema) })
+export const CreateOrderResSchema = z.object({ orders: z.array(OrderSchema), paymentId: z.number() })
 export const CancelOrderBodySchema = z.object({})
 export const CancelOrderResSchema = OrderSchema
 
--- ecom/src/routes/payment/payment.service.ts
+++ ecom/src/routes/payment/payment.service.ts
@@ -1,12 +1,34 @@
 import { Injectable } from '@nestjs/common'
 import { PaymentRepo } from './payment.repo'
 import { WebhookPaymentBodyType } from './payment.model'
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
+import { Server } from 'socket.io'
+import { SharedWebsocketRepository } from 'src/shared/repositories/shared-websocket.repo'
 
 @Injectable()
+@WebSocketGateway({ namespace: 'payment' })
 export class PaymentService {
-  constructor(private readonly paymentRepo: PaymentRepo) {}
+  @WebSocketServer()
+  server: Server
+  constructor(
+    private readonly paymentRepo: PaymentRepo,
+    private readonly sharedWebsocketRepository: SharedWebsocketRepository,
+  ) {}
 
-  receiver(body: WebhookPaymentBodyType) {
-    return this.paymentRepo.receiver(body)
+  async receiver(body: WebhookPaymentBodyType) {
+    const userId = await this.paymentRepo.receiver(body)
+    try {
+      const webSockets = await this.sharedWebsocketRepository.findMany(userId)
+      webSockets.forEach((ws) => {
+        this.server.to(ws.id).emit('payment', {
+          status: 'success',
+        })
+      })
+    } catch (error) {
+      console.log(error)
+      return {
+        message: 'Payment receiver successfully',
+      }
+    }
   }
 }
