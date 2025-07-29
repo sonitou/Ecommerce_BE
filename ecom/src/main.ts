d6a789c8000e0c1cc81f57531da53bc263bbfdd0
sonn30502 <sonn220619@gmail.com>
2025-07-29 15:09:41 +0700

    feat(security): thêm middleware Helmet để tăng cường bảo mật - Helmet được tích hợp để tự động cấu hình các HTTP Security Headers nhằm bảo vệ ứng dụng khỏi các tấn công phổ biến như XSS, clickjacking, và sniffing.

--- ecom/src/main.ts
+++ ecom/src/main.ts
@@ -5,9 +5,11 @@ import { UPLOAD_DIR } from './shared/constants/other.constants'
 import { WebsocketAdapter } from './websockets/websocket.adapter'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { patchNestJsSwagger } from 'nestjs-zod'
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  app.use(helmet())
   app.enableCors()
   app.set('trust proxy', 'loopback') // Trust requests from the loopback address
 
